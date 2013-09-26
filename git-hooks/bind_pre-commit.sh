24522ce649c3b689f6758b8c8a48e102376b5708
Roger Ignazio <me@rogerignazio.com>
2013-09-25 20:46:31 -0500

    initial commit bind_pre-commit.sh

---
+++ git-hooks/bind_pre-commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# BIND pre-commit hook for Git.
+# This file should be renamed and placed at '.git/hooks/pre-commit'
+
+# Check that the necessary tools are in our $PATH
+which named-checkzone > /dev/null 2>&1 || exit 1
+
+# Operate on each Puppet manifest (*.pp) in this repo
+for file in $(git diff --cached --name-only --diff-filter=ACM -- '*.db'); do
+    zone=`echo ${file} | perl -pe 's/(.*)\.db/$1/'`
+    named-checkzone $zone $file
+    [ $? != 0 ] && exit 1 || :
+done
+
+# No errors were reported, proceed with the commit
+exit 0
